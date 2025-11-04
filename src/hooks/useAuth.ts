79abbc64ba933691406b0c9f4ee673b2d82c9d79
weboloper <weboloper@gmail.com>
2025-11-04 20:31:10 +0300

    useauth removed

--- src/hooks/useAuth.ts
+++
@@ -1,5 +0,0 @@
-// src/hooks/useAuth.ts
-// Re-export useAuthStore for backward compatibility
-// This file can be removed if you prefer to import useAuthStore directly
-
-export { useAuthStore as useAuth } from '@/store/useAuthStore';
